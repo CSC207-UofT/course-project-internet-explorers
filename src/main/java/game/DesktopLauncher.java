4ac6370fe4fd43248a3412a3eec8e38ae62126dd
roylin2000 <roy0728@outlook.com>
2021-10-14 00:08:27 -0400

    Finished functionality of switching screens from main menu to game screen. Also packaged things a bit more.

--- src/main/java/game/DesktopLauncher.java
+++ src/main/java/game/DesktopLauncher.java
@@ -6,8 +6,14 @@ import game.GdxGame;
 
 class DesktopLauncher {
 
+
+
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.foregroundFPS = 60;
+        config.width = GdxGame.WIDTH;
+        config.height = GdxGame.HEIGHT;
+        config.resizable = false;
         new LwjglApplication(new GdxGame(), config);
     }
 }
