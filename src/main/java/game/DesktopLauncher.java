0e9b26dd46c028844f6f9a9149ff40dca01a7589
roylin2000 <roy0728@outlook.com>
2021-10-13 22:27:41 -0400

    Created new branch for working on main menu and screens. Also packaged some other code.

--- src/main/java/game/DesktopLauncher.java
+++ src/main/java/game/DesktopLauncher.java
@@ -1,5 +1,8 @@
+package game;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.GdxGame;
 
 class DesktopLauncher {
 
--- src/main/java/game/WorldEntity.java
+++ src/main/java/game/WorldEntity.java
@@ -1,3 +1,5 @@
+package game;
+
 public class WorldEntity{
 
 
