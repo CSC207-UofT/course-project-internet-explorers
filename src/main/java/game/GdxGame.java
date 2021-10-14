eee2aff00754d034460eb3aecedb8c6179c7a6ad
mdawess <64979871+mdawess@users.noreply.github.com>
2021-10-14 10:46:17 -0400

    Added rendering of PLayer, Enemy and Defender on screen

--- src/main/java/game/GdxGame.java
+++ src/main/java/game/GdxGame.java
@@ -1,39 +1,33 @@
 package game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.sun.tools.javac.Main;
-import game.screens.GameScreen;
 import game.screens.MainMenuScreen;
+import game.sprites.CharacterSprite;
+import game_characters.*;
+
+import java.util.ArrayList;
 
 public class GdxGame extends Game {
     public SpriteBatch batch;
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
-
+    CharacterSprite sprites = new CharacterSprite();
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         this.setScreen(new MainMenuScreen(this));
 
+        sprites.createCharacter();
+
     }
 
     @Override
     public void render() {
         super.render();
-
+        sprites.renderCharacter();
     }
 
     @Override
