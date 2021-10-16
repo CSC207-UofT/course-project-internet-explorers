16554fa8adab73b98f3134fd24c0c249e0d732e4
mdawess <64979871+mdawess@users.noreply.github.com>
2021-10-16 14:17:35 -0400

    Updated GameCharacter class to reflect changes to CRC and removed Player & NPC

--- src/main/java/game_characters/GameCharacter.java
+++ src/main/java/game_characters/GameCharacter.java
@@ -2,15 +2,16 @@ package game_characters;
 
 import java.util.ArrayList;
 
-public abstract class GameCharacter {
+public class GameCharacter {
     /*
-    * Abstract class that defines the main attributes of the classes Player, Enemy and Defender in the game.
+    * Class that defines the main attributes of the classes Player, Enemy and Defender in the game.
     * @param health: The current health the GameCharacter has
     * @param level: The level of the GameCharacter
     * @param position: The current position of the GameCharacter on the map
     * @param inventory: The Items the GameCharacter is able to use at a given point in the game
     * @param position: the x, y coordinates of the entity on the map
     * @param shape: the shape of the object, used to define movement and hit box.
+    * TODO: Extend WorldEntity class when implemented
     * */
 
     public String team;
@@ -18,25 +19,15 @@ public abstract class GameCharacter {
     public int level;
     // Need to change type to Item when implemented
     public ArrayList<String> inventory;
-    public double[][] position;
+    public float[] position;
     private final String shape;
 
-    public GameCharacter(int health, int level, ArrayList<String> inventory, String shape) {
+    public GameCharacter(String team, int health, int level, ArrayList<String> inventory, String shape) {
+        this.team = team;
         this.health = health;
         this.level = level;
         this.inventory = new ArrayList<String>();
         this.shape = shape;
-    }
-
-    public GameCharacter(String team, int health, int level, String shape) {
-        this.team = team;
-        this.health = health;
-        this.level = level;
-        this.shape = shape;
-    }
-
-    public String getShape() {
-        return this.shape;
     }
 
 }
