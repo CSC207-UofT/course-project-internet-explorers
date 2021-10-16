be66abbb4f2f5451e36d02239c0751cf98838a28
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2021-10-16 15:38:52 -0400

    move common menu logic to abstract class

--- src/main/java/core/screens/MainMenuScreen.java
+++ src/main/java/core/screens/MainMenuScreen.java
@@ -1,25 +1,14 @@
 package core.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import core.GdxGame;
 
-public class MainMenuScreen implements Screen {
-
-    private final Game game;
-    private final Stage stage;
+public class MainMenuScreen extends Menu {
 
     private static final int EXIT_BUTTON_WIDTH = 300;
     private static final int EXIT_BUTTON_HEIGHT = 150;
@@ -29,6 +18,12 @@ public class MainMenuScreen implements Screen {
     private static final int EXIT_BUTTON_Y = 100;
     private static final int PLAY_BUTTON_X = GdxGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2;
     private static final int PLAY_BUTTON_Y = 300;
+
+    public MainMenuScreen(GdxGame game) {
+        super(game);
+        stage.addActor(createPlayButton());
+        stage.addActor(createExitButton());
+    }
 
     private ImageButton createPlayButton() {
         ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
@@ -70,44 +65,5 @@ public class MainMenuScreen implements Screen {
         );
 
         return button;
-    }
-
-    public MainMenuScreen(GdxGame game) {
-        this.game = game;
-
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        stage.addActor(createPlayButton());
-        stage.addActor(createExitButton());
-    }
-
-    @Override
-    public void show() {}
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(delta);
-        stage.draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {}
-
-    @Override
-    public void pause() {}
-
-    @Override
-    public void resume() {}
-
-    @Override
-    public void hide() {}
-
-    @Override
-    public void dispose() {
-        stage.dispose();
     }
 }
--- src/main/java/core/screens/Menu.java
+++ src/main/java/core/screens/Menu.java
@@ -1,3 +1,50 @@
 package core.screens;
 
-public abstract class Menu {}
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import core.GdxGame;
+
+public abstract class Menu implements Screen {
+
+    protected final Game game;
+    protected final Stage stage;
+
+    public Menu(GdxGame game) {
+        this.game = game;
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
