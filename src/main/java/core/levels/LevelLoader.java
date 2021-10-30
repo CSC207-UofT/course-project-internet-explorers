2a5f8d88d647b45df703154876a8ab506dc81b03
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2021-10-30 14:54:46 -0400

    add basic level system

---
+++ src/main/java/core/levels/LevelLoader.java
@@ -0,0 +1,14 @@
+package core.levels;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class LevelLoader {
+
+    static LevelState getLevel1() {
+        LevelState lvl = new LevelState("maps/demo.tmx");
+        lvl.world = new World(new Vector2(0, 0), true);
+
+        return lvl;
+    }
+}
---
+++ src/main/java/core/levels/LevelState.java
@@ -0,0 +1,15 @@
+package core.levels;
+
+import com.badlogic.gdx.physics.box2d.World;
+import java.util.Date;
+
+public class LevelState {
+
+    private String mapPath;
+    protected World world;
+    protected Date startTime;
+
+    protected LevelState(String mapPath) {
+        this.mapPath = mapPath;
+    }
+}
