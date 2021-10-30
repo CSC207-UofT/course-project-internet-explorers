34da3e597f3d13b2b4ca5e10eb2857cdfffecc1f
roylin2000 <roy0728@outlook.com>
2021-10-30 19:01:23 -0400

    Starting new HUD branch and passed GdxGame game as a parameter to the screens

--- src/main/java/core/GdxGame.java
+++ src/main/java/core/GdxGame.java
@@ -1,6 +1,7 @@
 package core;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import core.characters.CharacterSprite;
 import core.characters.GameCharacter;
 import core.screens.menus.MainMenuScreen;
@@ -11,9 +12,13 @@ public class GdxGame extends Game {
 
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
+    public SpriteBatch batch;
 
     @Override
     public void create() {
+
+        this.batch = new SpriteBatch();
+
         this.setScreen(new MainMenuScreen(this));
     }
 
--- src/main/java/core/screens/menus/MainMenuScreen.java
+++ src/main/java/core/screens/menus/MainMenuScreen.java
@@ -23,9 +23,12 @@ public class MainMenuScreen extends Menu {
     private static final int EXIT_BUTTON_Y = 100;
     private static final int PLAY_BUTTON_X = GdxGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2;
     private static final int PLAY_BUTTON_Y = 300;
+    private final GdxGame game;
+
 
     public MainMenuScreen(GdxGame game) {
         super(game);
+        this.game = game;
         stage.addActor(createSpriteDemoButton());
         stage.addActor(createPlayButton());
         stage.addActor(createExitButton());
@@ -64,7 +67,7 @@ public class MainMenuScreen extends Menu {
             new ClickListener() {
                 @Override
                 public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                    game.setScreen(new GameScreen());
+                    game.setScreen(new GameScreen(game));
                     MainMenuScreen.super.dispose();
                 }
             }
