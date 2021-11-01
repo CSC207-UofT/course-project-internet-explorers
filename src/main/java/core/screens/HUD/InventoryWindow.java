ef3fcc24c47e4f3883de76f08707b2ab9c896da8
roylin2000 <roy0728@outlook.com>
2021-11-01 00:18:57 -0400

    Removed unused imports

--- src/main/java/core/screens/HUD/InventoryWindow.java
+++ src/main/java/core/screens/HUD/InventoryWindow.java
@@ -2,20 +2,10 @@ package core.screens.HUD;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Disposable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import core.screens.SpritesDemoScreen;
-import core.screens.menus.MainMenuScreen;
 
 public class InventoryWindow extends Window {
 
