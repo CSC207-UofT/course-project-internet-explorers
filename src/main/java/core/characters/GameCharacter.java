3ef861ae5232a91bc559443727364a53b005c0f9
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2021-11-02 13:24:07 -0400

    Overhauled CharacterManager to reflect Nov 1. TA meeting

--- src/main/java/core/characters/GameCharacter.java
+++ src/main/java/core/characters/GameCharacter.java
@@ -8,10 +8,8 @@ public class GameCharacter extends WorldEntity {
      * Class that defines the main attributes of the classes Player, Enemy and Defender in the game.
      * @param health: The current health the GameCharacter has
      * @param level: The level of the GameCharacter
-     * @param position: The current position of the GameCharacter on the map
+     * @param team: either player, defense or enemy to denote enemies and allies
      * @param inventory: The Items the GameCharacter is able to use at a given point in the game
-     * @param position: the x, y coordinates of the entity on the map
-     * @param shape: the shape of the object, used to define movement and hit box.
      * TODO: Change type of ArrayList to Item
      * */
 
@@ -20,12 +18,14 @@ public class GameCharacter extends WorldEntity {
     public int level;
     public ArrayList<String> inventory;
 
-    public GameCharacter(float[] position, String shape, String team, int health, int level,
+    public GameCharacter(String shape, String team, int health, int level,
                          ArrayList<String> inventory) {
-        super(position, shape);
+        super(shape);
         this.team = team;
         this.health = health;
         this.level = level;
         this.inventory = inventory;
     }
 }
+
+
