a767cac3468ac73162cc0bbcb1732d40d83fbef2
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2021-11-02 13:24:07 -0400

    Overhauled CharacterManager to reflect Nov 1. TA meeting

--- src/main/java/core/characters/WorldEntity.java
+++ src/main/java/core/characters/WorldEntity.java
@@ -3,7 +3,7 @@ package core.characters;
 public abstract class WorldEntity {
 
     /*
-     * Abstract class that defines the main attributes of the classes Player, Enemy and Defender in the game.
+     * Abstract class that defines the main attributes of the entities in the game.
      * @param position: the x, y coordinates of the entity on the map
      * @param shape: the shape of the object, used to define movement and hit box.
      * */
@@ -11,9 +11,9 @@ public abstract class WorldEntity {
     public float[] position;
     public String shape;
 
-    public WorldEntity(float[] position, String shape) {
+    public WorldEntity(String shape) {
 
-        this.position = position;
+        this.position = new float[]{0, 0};
         this.shape = shape;
     }
 }
