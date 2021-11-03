a04755dad85af0cbd687bb78e98ac1ba49ec85a4
CompTeam121 <rcctaddin@gmail.com>
2021-11-03 11:31:57 -0400

    add current InputHandler code

---
+++ src/main/java/core/characters/InputHandler.java
@@ -0,0 +1,14 @@
+package core.characters;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class InputHandler {
+    public void move(){
+
+    }
+}
