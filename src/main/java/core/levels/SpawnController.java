5a223ebe4d3afb8d0dd34e121c8e8e496b3ddf53
ianochano <ianyuyang.chang@mail.utoronto.ca>
2021-11-03 21:02:09 -0400

    created SpawnController

---
+++ src/main/java/core/levels/SpawnController.java
@@ -0,0 +1,26 @@
+package core.levels;
+
+import com.badlogic.gdx.math.Vector2;
+import core.world.WorldEntity;
+
+/**
+ * Controller class for spawning entities within the game
+ */
+
+
+public class SpawnController {
+
+    /**
+     * The x and y location where a WorldEntity should be spawned to
+     */
+    private Vector2 spawnLocation;
+
+    public SpawnController(Vector2 spawnLocation){
+        this.spawnLocation = spawnLocation;
+    }
+
+    public void spawn(WorldEntity worldEntity){
+        worldEntity.setPosition(this.spawnLocation);
+    }
+
+}
