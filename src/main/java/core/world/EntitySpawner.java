e1855eb844090d353ec18a4f13048ac4cf3a4721
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2021-11-04 21:51:26 -0400

    create EntitySpawner class

---
+++ src/main/java/core/world/EntitySpawner.java
@@ -0,0 +1,84 @@
+package core.world;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import java.util.function.Supplier;
+
+/**
+ * Command class used to spawn WorldEntities.
+ */
+public class EntitySpawner {
+
+    private WorldManager worldManager;
+
+    private Supplier<BodyDef> bodyDefSupplier;
+    private Supplier<FixtureDef[]> fixtureDefsSupplier;
+    private Supplier<WorldEntityGeometry> geometrySupplier;
+    private Supplier<Sprite> spriteSupplier;
+
+    /**
+     * Utility class used to specify position, size, and offset for new WorldEntities.
+     */
+    public static class WorldEntityGeometry {
+
+        public Vector2 position;
+        public Vector2 size;
+        public Vector2 offset;
+
+        public WorldEntityGeometry() {
+            this.position = new Vector2();
+            this.size = new Vector2();
+            this.offset = new Vector2();
+        }
+
+        public WorldEntityGeometry(Vector2 position, Vector2 size, Vector2 offset) {
+            this.position = position;
+            this.size = size;
+            this.offset = offset;
+        }
+    }
+
+    public EntitySpawner() {
+        bodyDefSupplier = BodyDef::new;
+        fixtureDefsSupplier = () -> new FixtureDef[] {};
+        geometrySupplier = WorldEntityGeometry::new;
+        spriteSupplier = () -> null;
+    }
+
+    public WorldEntity spawn() {
+        BodyDef bodyDef = bodyDefSupplier.get();
+        WorldEntityGeometry geometry = geometrySupplier.get();
+
+        bodyDef.position.set(geometry.position);
+
+        WorldEntity entity = worldManager.createEntity(bodyDef, fixtureDefsSupplier.get());
+
+        entity.setSize(geometry.size);
+        entity.setOffset(geometry.offset);
+        entity.setSprite(spriteSupplier.get());
+
+        return entity;
+    }
+
+    public void setWorldManager(WorldManager worldManager) {
+        this.worldManager = worldManager;
+    }
+
+    public void setBodyDefSupplier(Supplier<BodyDef> bodyDefSupplier) {
+        this.bodyDefSupplier = bodyDefSupplier;
+    }
+
+    public void setFixtureDefsSupplier(Supplier<FixtureDef[]> supplier) {
+        this.fixtureDefsSupplier = supplier;
+    }
+
+    public void geometrySupplier(Supplier<WorldEntityGeometry> supplier) {
+        this.geometrySupplier = supplier;
+    }
+
+    public void setSpriteSupplier(Supplier<Sprite> supplier) {
+        this.spriteSupplier = supplier;
+    }
+}
