185a7458e2a58b86d206382383a0e7b4f77f8b04
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2021-12-07 20:26:28 -0500

    refactor to enable further graphics-free testing

--- src/test/java/headless/HeadlessTests.java
+++ src/test/java/headless/HeadlessTests.java
@@ -1,4 +1,4 @@
-package presenters;
+package headless;
 
 import static org.mockito.Mockito.mock;
 
@@ -8,14 +8,19 @@ import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import core.config.Config;
 import core.presenters.levels.LevelGameplayController;
 import desktop.DesktopConfig;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class TestGraphicsToggle {
+/**
+ * Tests which need to run with graphics disabled.
+ */
+public class HeadlessTests {
 
     static final String graphicsSettingName = "render-graphics";
 
-    private void testRenderGraphicsSetting() {
+    private static void testRenderGraphicsSetting() {
         Assertions.assertNotNull(DesktopConfig.renderGraphics, "renderGraphics setting does not exist");
 
         Assertions.assertDoesNotThrow(() -> Config.get(graphicsSettingName), "Graphics rendering setting not found");
@@ -27,14 +32,12 @@ public class TestGraphicsToggle {
         Assertions.assertEquals(false, Config.get(graphicsSettingName), "Rendering setting not actually set to false");
     }
 
-    @Test
-    void testRunGameScreenWithoutGraphics() {
+    @BeforeAll
+    static void setup() {
         Gdx.gl20 = mock(com.badlogic.gdx.graphics.GL20.class);
         Gdx.gl = Gdx.gl20;
 
         testRenderGraphicsSetting();
-
-        LevelGameplayController levelGameplayController = new LevelGameplayController();
 
         Gdx.app =
             new HeadlessApplication(
@@ -43,12 +46,20 @@ public class TestGraphicsToggle {
                     public void create() {}
                 }
             );
+    }
+
+    @AfterAll
+    static void teardown() {
+        Gdx.app.exit();
+    }
+
+    @Test
+    void testLevelGameplayController() {
+        LevelGameplayController levelGameplayController = new LevelGameplayController();
 
         levelGameplayController.show();
         levelGameplayController.render(0.1f);
         levelGameplayController.hide();
         levelGameplayController.dispose();
-
-        Gdx.app.exit();
     }
 }
