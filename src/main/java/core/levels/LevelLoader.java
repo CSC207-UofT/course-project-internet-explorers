3f848cc4f4e6bd299219bf67de6ef71a4fe0c24b
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2021-12-08 23:30:53 -0500

    minor tweaks

--- src/main/java/core/levels/LevelLoader.java
+++ src/main/java/core/levels/LevelLoader.java
@@ -20,8 +20,6 @@ public class LevelLoader {
     public static final int LEVEL_2_SPAWN_INTERVAL = 10;
     public static final int LEVEL_3_SPAWN_INTERVAL = 5;
 
-    // TODO map path
-
     /**
      * If savedState file exists for the specified level, load SavedLevel given saved information
      * If no savedState file exists, load SavedLevel given default information for user chosen
